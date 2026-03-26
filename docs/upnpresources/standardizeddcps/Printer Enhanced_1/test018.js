97c04ddb73e3b29fdf6f9d2b3837a21705ac8906
TJ <bjtj10@gmail.com>
2026-03-26 14:56:44 +0900

    /docs

---
+++ docs/upnpresources/standardizeddcps/Printer Enhanced_1/test018.js
@@ -0,0 +1,33 @@
+//
+// A JScript for validating a file with the UPnP schemata.
+//
+// Copyright (c) 2004, 2005 Hewlett-Packard Development Company, L.P.
+// All Rights Reserved.
+//
+var sOutput = validateFile("test018.xml");
+
+//
+//
+//
+WScript.Echo(sOutput);
+
+//
+//
+//
+function validateFile(strFile) {
+	var xs = new ActiveXObject("MSXML2.XMLSchemaCache.4.0");
+	xs.add("urn:schemas-upnp-org:service:PrintEnhanced:1", "PrintEnhanced.xsd");
+
+	var xd = new ActiveXObject("MSXML2.DOMDocument.4.0");
+	xd.schemas = xs;
+	xd.async = false;
+	xd.validateOnParse = true;
+	xd.resolveExternals = true;
+	xd.load(strFile);
+
+	if (xd.parseError.errorCode != 0) {
+		return ("An error occured in validating \"" + strFile + "\"\n\n" + strFile + "(" + xd.parseError.line + "): " + xd.parseError.srcText + "\n\n" + "error: " + xd.parseError.reason);
+	} else {
+		 return("\"" + strFile + "\" is valid.\n");
+	}
+}
